5fa0bf97fbdeb122b8ff1402b114cc74c28fbf48
Ranor80 <bvasilj@gmail.com>
2022-04-26 15:08:15 +0200

    error fix

--- main.js
+++ main.js
@@ -1,52 +1,68 @@
 const buttons = document.querySelectorAll("button");
 const screen = document.querySelector(".screen");
-
+let blackBtn = document.getElementById('black');
+let eraserBtn = document.getElementById('eraser');
+let colorBtn = document.getElementById('colors');
+let resizeBtn = document.getElementById('resize');
+let resetBtn = document.getElementById('reset')
+let color = "black";
+let click = true;
 let pixel; 
 let gridsize = 50;
 
 function drawGrid (screenSize) {
-  for (let i=0; i < screenSize**2; i++) {
-    pixel = document.createElement('div');
-    pixel.classList.add("pixel");
-    pixel.addEventListener('mouseover', () => {
-      pixel.style.backgroundColor = 'black';
-    })
-    pixel.style.backgroundColor = 'white'
-    screen.appendChild(pixel);
-  }
-  screen.style.gridTemplateColumns = `repeat(${screenSize}, auto)`;
-  screen.style.gridTemplateRows = `repeat(${screenSize}, auto)`;
-}
-
-  drawGrid(2);
-
-  function clear (request) {
-    if(request === 'resize'){
-      gridsize = prompt('please enter a new grid size of not more than 100', 50);
-      if(gridsize > 100 || gridsize === null){
-      gridsize = 100;
-    }
-    }
-    screen.innerHTML = '';
-    drawGrid(gridsize);
-  }
-
-
-
-
-
-/*
-function drawGrid (screenSize) {
-  for(i = 0; i < screenSize ** 2; i++) {
-    pixel = document.createElement('div')
+  for(let i = 0; i < screenSize ** 2; i++) {
+    let pixel = document.createElement('div')
     pixel.classList.add("pixel");
     pixel.style.backgroundColor = 'white';
     screen.appendChild(pixel);
-    
   }
-  screen.style.gridTemplateColumns = `repeat(${screenSize}, auto)`; 
+  screen.style.gridTemplateColumns =  `repeat(${screenSize}, auto)`;
   screen.style.gridTemplateRows =  `repeat(${screenSize}, auto)`;
-  
 }
 
-drawGrid(5);  */
+  drawGrid(16);
+
+function colorWhite() {
+  let pixels = document.querySelectorAll('.pixel');
+  pixels.forEach(pxl => {
+    pxl.addEventListener('mouseover', (e) => {
+      pxl.style.backgroundColor = "white"
+    })
+  })
+}
+
+function colorBlack() {
+  let pixels = document.querySelectorAll('.pixel');
+  pixels.forEach(pxl => {
+    pxl.addEventListener('mouseover', (e) => {
+      pxl.style.backgroundColor = "black"
+    })
+  })
+}
+
+function colorRandom() {
+  let pixels = document.querySelectorAll('.pixel');
+  pixels.forEach(pxl => {
+    pxl.addEventListener('mouseover', (e) => {
+      pxl.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+    })
+  })
+}
+
+function resize() {
+  gridsize = prompt('please enter a new grid size of not more than 100, or lower than 2', 50);
+  if(gridsize > 100 || gridsize === null || gridsize < 2){
+  gridsize = 16;
+}
+screen.innerHTML = '';
+drawGrid(gridsize);
+}
+
+function reset() {
+  let pixels = document.querySelectorAll('.pixel');
+  pixels.forEach(pxl => {
+    pxl.style.backgroundColor = "white";
+  })
+}
+
